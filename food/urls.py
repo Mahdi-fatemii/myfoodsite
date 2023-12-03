aa39a484e0eb5579f58c3e0313945c7a6256c05b
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-12-03 16:07:56 +0330

    first commit

---
+++ food/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+
+app_name = 'food'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:item_id>/', views.detail, name='detail'),
+    path('item/', views.item, name='item'),
+]
---
+++ food/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+
+
+# Create your views here.
+
+
+def index(request):
+    item_list = Item.objects.all()
+    context = {
+        'item_list': item_list,
+    }
+    return render(request, 'food/index.html', context)
+
+
+def item(request):
+    return HttpResponse('this is an item view')
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'food/detail.html', context)
