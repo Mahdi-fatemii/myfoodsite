457dd7d95828132eb01f65ac3717fa425a6fa4b9
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-12-06 14:10:23 +0330

    adding form to add items and forms.py & item-for.html

--- food/urls.py
+++ food/urls.py
@@ -4,7 +4,11 @@ from django.urls import path
 
 app_name = 'food'
 urlpatterns = [
+    # /food/
     path('', views.index, name='index'),
+    # /food/1
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
+    # add item
+    path('add', views.create_item, name='create_item'),
 ]
--- food/views.py
+++ food/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import ItemForm
 from .models import Item
 from django.template import loader
-
-
 # Create your views here.
 
 
@@ -26,3 +25,13 @@ def detail(request, item_id):
     }
 
     return render(request, 'food/detail.html', context)
+
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'food/item-form.html', {'form': form})
