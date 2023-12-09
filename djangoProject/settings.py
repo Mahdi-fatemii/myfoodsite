32afb1bc5ed9b94f13274337f9be09ff712ad5be
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-12-09 19:30:51 +0330

    creating user registration form

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'food.apps.FoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
