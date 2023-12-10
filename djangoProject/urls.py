54404543f0438b0e21203b9a83dc644ac36047c6
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-12-10 12:31:31 +0330

    complete login/logout functionality

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as authentication_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
     path('register/', user_views.register, name='register'),
+    path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -10,8 +10,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Welcome {username}, your account is created.')
-            return redirect('food:index')
+            messages.success(request, f'Welcome {username}.')
+            return redirect('login')
     else:
         form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
