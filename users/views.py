022e3e6356d9356d0051692718b8857ae914becf
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-12-10 11:43:46 +0330

    saving users / little fix on html messages

--- users/views.py
+++ users/views.py
@@ -8,6 +8,7 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}, your account is created.')
             return redirect('food:index')
